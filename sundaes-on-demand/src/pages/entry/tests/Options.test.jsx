7efdca8883100fef6ff3ec323a5efcf35cb010a1
Patrick <patricktsaikl@gmail.com>
2023-01-13 10:25:19 +0000

    toppingOption test and implementation

--- sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
+++ sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
@@ -1,4 +1,4 @@
-import { redner, render, screen } from "@testing-library/react";
+import { findAllByRole, redner, render, screen } from "@testing-library/react";
 import Options from "../Option";
 
 test("displays image for each scoop option from server", async () => {
@@ -10,4 +10,16 @@ test("displays image for each scoop option from server", async () => {
   // confirm alt text of images
   const allText = scoopImages.map((element) => element.alt);
   expect(allText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+test("displays image for each tooping option from server", async () => {
+  render(<Options optionType="toppings" />);
+  const toppingImgs = await screen.findAllByRole("img", { name: /topping$/i });
+  expect(toppingImgs).toHaveLength(3);
+  const allText = toppingImgs.map((item) => item.alt);
+  expect(allText).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
 });
